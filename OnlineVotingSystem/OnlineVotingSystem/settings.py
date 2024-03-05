45de1622a0eb2f4039c058da700f66491647ea26
Muhammad Saad Hassan <162106714+Saad-Hassan25@users.noreply.github.com>
2024-03-05 17:39:33 +0500

    Update settings.py

--- OnlineVotingSystem/OnlineVotingSystem/settings.py
+++ OnlineVotingSystem/OnlineVotingSystem/settings.py
@@ -138,10 +138,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'l215252@lhr.nu.edu.pk'  # Your Gmail email address
-EMAIL_HOST_PASSWORD = '@!25Saad25!@'  # Your Gmail password or app-specific password
-DEFAULT_FROM_EMAIL = 'l215252@lhr.nu.edu.pk'  # Configure this
-
+EMAIL_HOST_USER = 'example@example.com'  # Your Gmail email address
+EMAIL_HOST_PASSWORD = 'YourPassword'  # Your Gmail password or app-specific password
+DEFAULT_FROM_EMAIL = 'example@example.com'  # Same as EMAIL_HOST_USER
 JAZZMIN_UI_TWEAKS = {
     "theme": "darkly",
 }
